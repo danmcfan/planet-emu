680b1163b40a69d3f033a17f748025d0ac0edf2c
danny <danmcfan33@gmail.com>
2022-03-09 13:42:51 +0000

    update cdk stack with template function and role

--- src/cdk_stack.py
+++ src/cdk_stack.py
@@ -1,13 +1,23 @@
 import aws_cdk as core
-from aws_cdk import aws_lambda as lambda_
+from aws_cdk import aws_lambda as lambda_, aws_iam as iam
 from constructs import Construct
 
-class ExecutionStack(core.Stack):
 
+class ExecutionStack(core.Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        genesis_point_function = lambda_.Function(
-            self, "genesis-point-function",
-            function_name="genesis-point",
+        function = lambda_.Function(
+            self,
+            "function",
+            function_name="function-name",
+            runtime=lambda_.Runtime.PYTHON_3_7,
+            handler="main.handler",
+            code=lambda_.Code.from_asset("src"),
+            timeout=core.Duration.seconds(60),
+            memory_size=128,
+        )
+
+        function.role.add_managed_policy(
+            iam.ManagedPolicy.from_aws_managed_policy_name("FullS3Access")
         )
