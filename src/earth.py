d515b347ce0e548c9334350d0cd8577883e8a31a
danny <danmcfan33@gmail.com>
2022-03-14 04:49:53 +0000

    add get cropland info function

--- src/earth.py
+++ src/earth.py
@@ -6,13 +6,13 @@ ee.Initialize()
 
 
 def get_weather_df(x: float, y: float, year: int) -> pd.DataFrame:
-    l8 = ee.ImageCollection("NASA/ORNL/DAYMET_V4")
+    img = ee.ImageCollection("NASA/ORNL/DAYMET_V4")
     point = ee.Geometry.Point([x, y])
 
-    l8_filtered = l8.filterBounds(point).filterDate(
+    img_filtered = img.filterBounds(point).filterDate(
         f"{year}-01-01", f"{year+1}-01-01"
     )
-    bands = l8_filtered.toBands()
+    bands = img_filtered.toBands()
 
     features = bands.sample(
         region=point, geometries=True, scale=1000
@@ -35,5 +35,37 @@ def get_weather_df(x: float, y: float, year: int) -> pd.DataFrame:
     df = pd.DataFrame(rows)
 
     df = df.pivot(index="date", columns="band", values="value").reset_index()
+
+    return df
+
+
+def get_crop_df(x: float, y: float) -> pd.DataFrame:
+    img = ee.ImageCollection("USDA/NASS/CDL")
+    point = ee.Geometry.Point([x, y])
+
+    img_filtered = img.filterBounds(point)
+    bands = img_filtered.toBands()
+
+    features = bands.sample(region=point, geometries=True, scale=30).getInfo()[
+        "features"
+    ]
+
+    if len(features) != 1:
+        raise ValueError(
+            f"Number of features is greater than one: {len(features)}"
+        )
+
+    feature = features[0]
+
+    rows = list()
+
+    for k, v in feature["properties"].items():
+        year, band = k.split("_")
+        year = int(year.replace("a", ""))
+        rows.append(dict(year=year, band=band, value=v))
+
+    df = pd.DataFrame(rows)
+
+    df = df.pivot(index="year", columns="band", values="value").reset_index()
 
     return df
