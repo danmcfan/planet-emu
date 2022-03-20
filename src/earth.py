0d7961c0e1f302a846637de56e313fb8b6045010
danny <danmcfan33@gmail.com>
2022-03-20 18:13:19 +0000

    sample mean over time period by region instead of single point

--- src/earth.py
+++ src/earth.py
@@ -1,90 +1,77 @@
 import ee
+import eeconvert
+import geopandas as gpd
 import pandas as pd
 import datetime as dt
-from pprint import pprint
+import time
 
 ee.Initialize()
 
 
-def get_weather_df(x: float, y: float, year: int) -> pd.DataFrame:
-    img = ee.ImageCollection("NASA/ORNL/DAYMET_V4")
-    point = ee.Geometry.Point([x, y])
-
-    img_filtered = img.filterBounds(point).filterDate(
-        f"{year}-01-01", f"{year+1}-01-01"
-    )
-    bands = img_filtered.toBands()
-
-    features = bands.sample(
-        region=point, geometries=True, scale=1000
-    ).getInfo()["features"]
-
-    if len(features) != 1:
-        raise ValueError(
-            f"Number of features is greater than one: {len(features)}"
-        )
-
-    feature = features[0]
-
-    rows = list()
-
-    for k, v in feature["properties"].items():
-        date, band = k.split("_")
-        date = dt.datetime.strptime(date, "%Y%m%d")
-        rows.append(dict(date=date, band=band, value=v))
-
-    df = pd.DataFrame(rows)
-
-    df = df.pivot(index="date", columns="band", values="value").reset_index()
-
-    return df
+IMAGE_COLLECTION_NAMES = {
+    "weather": "NASA/ORNL/DAYMET_V4",
+    "reflectance": "MODIS/006/MOD09Q1",
+    "crop": "USDA/NASS/CDL",
+}
 
 
-def get_crop_df(x: float, y: float) -> pd.DataFrame:
-    img = ee.ImageCollection("USDA/NASS/CDL")
-    point = ee.Geometry.Point([x, y])
+def get_mean_image_sample(
+    img_collection: str,
+    in_gdf: gpd.GeoDataFrame,
+    scale: int,
+    month: int,
+    year: int = 2020,
+) -> pd.DataFrame:
+    in_fc = eeconvert.gdfToFc(in_gdf)
 
-    img_filtered = img.filterBounds(point)
-    bands = img_filtered.toBands()
+    next_month = month + 1
 
-    features = bands.sample(region=point, geometries=True, scale=30).getInfo()[
-        "features"
-    ]
+    if next_month > 12:
+        next_month = 1
+        next_year = year + 1
+    else:
+        next_year = year
 
-    if len(features) != 1:
-        raise ValueError(
-            f"Number of features is greater than one: {len(features)}"
-        )
-
-    feature = features[0]
-
-    rows = list()
-
-    for k, v in feature["properties"].items():
-        year, band = k.split("_")
-        year = int(year.replace("a", ""))
-        rows.append(dict(year=year, band=band, value=v))
-
-    df = pd.DataFrame(rows)
-
-    df = df.pivot(index="year", columns="band", values="value").reset_index()
-
-    return df
-
-
-def get_sentinel_fc(fc: ee.FeatureCollection, scale: int) -> pd.DataFrame:
     img = (
-        ee.ImageCollection("USDA/NASS/CDL")
-        .sort("system:time_start", False)
-        .first()
+        ee.ImageCollection(img_collection)
+        .filterDate(f"{year}-{month}-01", f"{next_year}-{next_month}-01")
+        .filterBounds(in_fc)
+        .mean()
     )
 
-    fc = img.sampleRegions(
-        fc,
+    out_fc = img.sampleRegions(
+        in_fc,
         properties=[],
         projection=ee.Projection("EPSG:4326"),
         scale=scale,
         geometries=True,
     )
 
-    return fc
+    out_gdf = eeconvert.fcToGdf(out_fc)
+
+    return out_gdf, img
+
+
+def export_to_drive(
+    image: ee.Image,
+    file_name: str,
+    in_gdf: gpd.GeoDataFrame,
+    scale: int,
+    delay: int = 10,
+) -> None:
+
+    task = ee.batch.Export.image.toDrive(
+        image=image,
+        description=file_name,
+        folder="exports",
+        scale=scale,
+        region=eeconvert.gdfToFc(in_gdf),
+    )
+
+    task.start()
+
+    while task.active():
+        print("Task is active...")
+        time.sleep(delay)
+
+    return task
