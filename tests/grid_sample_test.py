9a7461853360ade08afefaedc628f79292bd2408
danny <danmcfan33@gmail.com>
2022-04-01 03:51:40 +0000

    remove exporting of csv and png

--- tests/grid_sample_test.py
+++ tests/grid_sample_test.py
@@ -1,33 +1,17 @@
-from shapely import geometry
-from matplotlib import pyplot as plt
-
+from tests.util import POLYGON
 from src import grid_sample as gs
 
-EXPORT = False
 SPLIT = 4
 COUNT = SPLIT**2
 
 
-def test_get_grid_gdf():
-    gdf = gs.get_grid_gdf(-95, 35, 85, 45, SPLIT)
-    gdf.plot(facecolor="none", edgecolor="black")
-
-    if EXPORT:
-        gdf.to_csv(".temp/grid.csv")
-        plt.savefig(".temp/grid.png")
+def test_create_grid_gdf():
+    gdf = gs.create_grid_gdf(POLYGON, SPLIT)
 
     assert gdf.shape == (COUNT, 1)
 
 
 def test_divide_polygon():
-    centroid = geometry.Point([-90, 40])
-    polygon = centroid.buffer(0.01)
-
-    gdf = gs.divide_polygon(polygon, SPLIT)
-    gdf.plot(facecolor="none", edgecolor="black")
-
-    if EXPORT:
-        gdf.to_csv(".temp/divide.csv")
-        plt.savefig(".temp/divide.png")
+    gdf = gs.divide_polygon(POLYGON, SPLIT)
 
     assert gdf.shape == (COUNT, 1)
