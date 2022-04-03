7cc3844b69f68140532576c15ae0df9e0edb4338
danny <danmcfan33@gmail.com>
2022-04-03 04:19:20 +0000

    load secrets for init

--- tests/earth_test.py
+++ tests/earth_test.py
@@ -1,9 +1,16 @@
+import os
+from dotenv import load_dotenv
+
 from tests.util import GDF
 from src import earth
 
+load_dotenv()
+NAME = os.getenv("GCP_SERVICE_NAME")
+PROJECT = os.getenv("GCP_PROJECT")
+
 
 def test_get_mean_image_sample():
-    earth.init()
+    earth.init(NAME, PROJECT)
 
     ic_name = earth.IMAGE_COLLECTION_NAMES.get("weather")
 
