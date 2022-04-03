d2f20de7d077f0fa2f1c97619c619048d94bdfab
danny <danmcfan33@gmail.com>
2022-04-03 04:10:27 +0000

    uncomment and add init

--- tests/earth_test.py
+++ tests/earth_test.py
@@ -1,15 +1,17 @@
-# from tests.util import GDF
-# from src import earth
+from tests.util import GDF
+from src import earth
 
 
-# def test_get_mean_image_sample():
-#     ic_name = earth.IMAGE_COLLECTION_NAMES.get("weather")
+def test_get_mean_image_sample():
+    earth.init()
 
-#     sample_gdf = earth.get_mean_image_sample(
-#         ic_name,
-#         GDF,
-#         year=2020,
-#         scale=1000,
-#     )
+    ic_name = earth.IMAGE_COLLECTION_NAMES.get("weather")
 
-#     assert sample_gdf.shape == (4, 8)
+    sample_gdf = earth.get_mean_image_sample(
+        ic_name,
+        GDF,
+        year=2020,
+        scale=1000,
+    )
+
+    assert sample_gdf.shape == (4, 8)
