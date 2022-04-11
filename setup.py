f0ccdb0f8d09689d4d3e5ec34820e95f633131e6
danny <danmcfan33@gmail.com>
2022-04-11 16:49:23 +0000

    add planet emu package

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name="planet_emu",
+    version="0.1.0",
+    description="Planet Emu",
+    author="Danny OBrien",
+    author_email="danmcfan33@gmail.com",
+    packages=["planet_emu"],
+)
