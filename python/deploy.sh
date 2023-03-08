9d6dffb7d5e9cb1a525925e5d0e6d5cd2f661d0f
Danny OBrien <danmcfan33@gmail.com>
2023-03-07 22:02:20 -0800

    add build arg

--- python/deploy.sh
+++ python/deploy.sh
@@ -1,2 +1,2 @@
-docker build -t ghcr.io/danmcfan/planet-emu-fast-api:latest . \
+docker build --build-arg DECRYPT_PASSWORD -t ghcr.io/danmcfan/planet-emu-fast-api:latest . \
     && docker push ghcr.io/danmcfan/planet-emu-fast-api:latest
