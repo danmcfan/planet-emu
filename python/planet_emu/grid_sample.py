cbf80fe9e1684e992cd34125ad3b76ae944e545e
Danny OBrien <danmcfan33@gmail.com>
2023-03-08 15:54:44 -0800

    change grid sample to absolute units

--- python/planet_emu/grid_sample.py
+++ python/planet_emu/grid_sample.py
@@ -3,32 +3,30 @@ import numpy as np
 from shapely import geometry
 
 
-def create_grid_gdf(polygon: geometry.Polygon, split: int = 2) -> gpd.GeoDataFrame:
-    min_x, min_y, max_x, max_y = polygon.bounds
-
-    x_list = np.linspace(min_x, max_x, split + 1)
-    y_list = np.linspace(min_y, max_y, split + 1)
-
-    rectangles = [
-        geometry.box(
-            x_list[i],
-            y_list[j],
-            x_list[i + 1],
-            y_list[j + 1],
-        )
-        for i in range(split)
-        for j in range(split)
-    ]
-
-    return gpd.GeoDataFrame(geometry=rectangles, crs="EPSG:4326")
-
-
-def divide_polygon(polygon: geometry.Polygon, split: int = 2) -> gpd.GeoDataFrame:
-    grid_gdf = create_grid_gdf(polygon, split)
+def divide_polygon(polygon: geometry.Polygon, resolution: float) -> gpd.GeoDataFrame:
+    grid_gdf = create_grid_gdf(polygon, resolution)
 
     polygon_gdf = gpd.GeoDataFrame(
-        geometry=grid_gdf.intersection(polygon), crs="EPSG:4326"
-    )
+        geometry=grid_gdf.intersection(polygon), crs="EPSG:3857"
+    )  # type: ignore
     polygon_gdf = polygon_gdf.loc[~polygon_gdf.geometry.is_empty].reset_index(drop=True)
 
     return polygon_gdf
+
+
+def create_grid_gdf(polygon: geometry.Polygon, resolution: float) -> gpd.GeoDataFrame:
+    min_x, min_y, max_x, max_y = polygon.bounds
+
+    rectangles = []
+
+    x = min_x
+    y = min_y
+
+    while x < max_x:
+        while y < max_y:
+            rectangles.append(geometry.box(x, y, x + resolution, y + resolution))
+            y += resolution
+        x += resolution
+        y = min_y
+
+    return gpd.GeoDataFrame(geometry=rectangles, crs="EPSG:3857")  # type: ignore
