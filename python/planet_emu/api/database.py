d1ce6bc96c1374b3e76949ca17765fb3f127bfdd
Danny OBrien <danmcfan33@gmail.com>
2023-03-07 19:59:40 -0800

    update api to use postgresql database

---
+++ python/planet_emu/api/database.py
@@ -0,0 +1,17 @@
+import os
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+SQLALCHEMY_DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL")
+
+if SQLALCHEMY_DATABASE_URL is None:
+    raise ValueError("SQLALCHEMY_DATABASE_URL is not set")
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
---
+++ python/planet_emu/api/main.py
@@ -0,0 +1,120 @@
+from uuid import uuid4
+
+from fastapi import FastAPI, HTTPException, Depends
+from fastapi.middleware.cors import CORSMiddleware
+
+
+from sqlalchemy.orm import Session
+
+from planet_emu.api import crud, models, schemas, util
+from planet_emu.api.database import SessionLocal, engine
+
+models.Base.metadata.create_all(bind=engine)
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+app = FastAPI(
+    title="planet-emu-api",
+    description="Planet Emulator API",
+    version="1.0.0",
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+    allow_credentials=True,
+)
+
+
+@app.get("/")
+def index():
+    return {
+        "message": "Go to https://api.planet-emu.com/docs for complete API documentation."
+    }
+
+
+@app.post("/submit/")
+def submit_job(point: schemas.Point):
+    if not (-124.763068 <= point.x <= -66.949895):
+        return {"point": point, "error": "X coordinate is out of range"}
+    if not (24.523096 <= point.y <= 49.384358):
+        return {"point": point, "error": "Y coordinate is out of range"}
+
+    job_id = uuid4().hex[:8]
+
+    data = util.invoke_job(point.x, point.y, job_id)
+
+    return data
+
+
+@app.get("/status/{job_id}")
+def get_status(job_id: str):
+    data = util.get_json(job_id)
+
+    if not data:
+        raise HTTPException(400, "Job not found")
+
+    return {
+        "job_id": job_id,
+        "status": data[0]["status"],
+    }
+
+
+@app.get("/results/{job_id}")
+def get_results(job_id: str):
+    data = util.get_json(job_id)
+
+    if not data:
+        raise HTTPException(400, "Job not found")
+
+    if data[0]["status"] == "pending":
+        raise HTTPException(400, "Job is still pending")
+
+    return {
+        "job_id": job_id,
+        "data": data[0],
+    }
+
+
+@app.post("/users/", response_model=schemas.User)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    db_user = crud.get_user_by_email(db, email=user.email)
+    if db_user:
+        raise HTTPException(status_code=400, detail="Email already registered")
+    return crud.create_user(db=db, user=user)
+
+
+@app.get("/users/", response_model=list[schemas.User])
+def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    users = crud.get_users(db, skip=skip, limit=limit)
+    return users
+
+
+@app.get("/users/{user_id}", response_model=schemas.User)
+def read_user(user_id: int, db: Session = Depends(get_db)):
+    db_user = crud.get_user(db, user_id=user_id)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return db_user
+
+
+@app.post("/users/{user_id}/items/", response_model=schemas.Item)
+def create_item_for_user(
+    user_id: int, item: schemas.ItemCreate, db: Session = Depends(get_db)
+):
+    return crud.create_user_item(db=db, item=item, user_id=user_id)
+
+
+@app.get("/items/", response_model=list[schemas.Item])
+def read_items(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    items = crud.get_items(db, skip=skip, limit=limit)
+    return items
