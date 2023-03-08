21e588fdde9d38f85953aaeb85f15e156d43be27
Danny OBrien <danmcfan33@gmail.com>
2023-03-07 22:15:10 -0800

    add SQLALCHEMY_DATABASE_URL env var

--- python/deploy.sh
+++ python/deploy.sh
@@ -1,2 +1,5 @@
-docker build --build-arg DECRYPT_PASSWORD -t ghcr.io/danmcfan/planet-emu-fast-api:latest . \
-    && docker push ghcr.io/danmcfan/planet-emu-fast-api:latest
+docker build \
+    --build-arg DECRYPT_PASSWORD \
+    --build-arg SQLALCHEMY_DATABASE_URL \
+    -t ghcr.io/danmcfan/planet-emu-fast-api:latest . \
+&& docker push ghcr.io/danmcfan/planet-emu-fast-api:latest
