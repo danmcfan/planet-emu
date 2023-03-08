8d8b8906a11a54e17371c0114d1fdba90931f807
Danny OBrien <danmcfan33@gmail.com>
2023-03-08 14:19:48 -0800

    add new icon for FastAPI docs

--- svelte/src/components/Navigation.svelte
+++ svelte/src/components/Navigation.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
-    import "iconify-icon";
+    import Icon from "@iconify/svelte";
 
+    const apiLink = "https://api.planetemu.com/docs";
     const githubLink = "https://github.com/danmcfan/planet-emu";
 </script>
 
@@ -18,15 +19,26 @@
             Planet Emu
         </div>
     </div>
-    <a
-        class="group mx-4 sm:mx-8 flex flex-row justify-center items-center hover:cursor-pointer"
-        href={githubLink}
-    >
-        <iconify-icon
-            icon="mdi:github"
-            class="text-white group-hover:text-black text-[2rem] sm:text-[2.5rem]"
-        />
-    </a>
+    <div class="flex flow-row justify-around items-center">
+        <a
+            class="group mx-4 sm:mx-8 flex flex-row justify-center items-center hover:cursor-pointer"
+            href={apiLink}
+        >
+            <Icon
+                icon="simple-icons:fastapi"
+                class="text-white group-hover:text-black text-[1.75rem] sm:text-[2.25rem]"
+            />
+        </a>
+        <a
+            class="group mx-4 sm:mx-8 flex flex-row justify-center items-center hover:cursor-pointer"
+            href={githubLink}
+        >
+            <Icon
+                icon="carbon:logo-github"
+                class="text-white group-hover:text-black text-[2rem] sm:text-[2.5rem]"
+            />
+        </a>
+    </div>
 </div>
 
 <style>
