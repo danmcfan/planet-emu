d5fdb52c280c1d9fc5438e84e08d8222cd6adb48
Danny OBrien <danmcfan33@gmail.com>
2023-03-08 12:31:25 -0800

    result model

--- python/planet_emu/api/models.py
+++ python/planet_emu/api/models.py
@@ -1,14 +1,13 @@
-from sqlalchemy import JSON, Column, Enum, Float, String
+from sqlalchemy import JSON, Column, Float, String, Integer
 
 from planet_emu.api.database import Base
-from planet_emu.api.schemas import Status
 
 
-class Job(Base):
-    __tablename__ = "jobs"
+class Result(Base):
+    __tablename__ = "results"
 
     id = Column(String, primary_key=True, index=True)
     x = Column(Float)
     y = Column(Float)
-    status = Column(Enum(Status), default=Status.PENDING)
-    result = Column(JSON, nullable=True)
+    year = Column(Integer)
+    properties = Column(JSON)
