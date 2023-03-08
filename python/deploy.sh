7c05cb31800b6439b65d355fe18ec0224741243c
Danny OBrien <danmcfan33@gmail.com>
2023-03-08 09:38:23 -0800

    add GCP env vars

--- python/deploy.sh
+++ python/deploy.sh
@@ -1,5 +1,7 @@
 docker build \
-    --build-arg DECRYPT_PASSWORD \
     --build-arg SQLALCHEMY_DATABASE_URL \
+    --build-arg GCP_SERVICE_NAME \
+    --build-arg GCP_PROJECT \
+    --build-arg DECRYPT_PASSWORD \
     -t ghcr.io/danmcfan/planet-emu-fast-api:latest . \
 && docker push ghcr.io/danmcfan/planet-emu-fast-api:latest
