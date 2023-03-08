91f1705c8c1352f92f1e4aaa2df5f063a8b18d65
Danny OBrien <danmcfan33@gmail.com>
2023-03-08 12:31:37 -0800

    task and result schemas

--- python/planet_emu/api/schemas.py
+++ python/planet_emu/api/schemas.py
@@ -1,36 +1,23 @@
-from enum import Enum
-from typing import Any
+from typing import Any, Hashable
 
 from pydantic import BaseModel
-
-
-class Point(BaseModel):
-    x: float
-    y: float
-
-
-class Status(str, Enum):
-    PENDING = "pending"
-    RUNNING = "running"
-    SUCCESS = "success"
-    FAILURE = "failure"
-
-
-class Job(BaseModel):
-    id: str
-    x: float
-    y: float
-    status: Status = Status.PENDING
-    result: dict[str, float] | None = None
-
-    class Config:
-        orm_mode = True
 
 
 class Task(BaseModel):
     id: str
     status: str
     result: Any
+
+    class Config:
+        orm_mode = True
+
+
+class Result(BaseModel):
+    id: str
+    x: float
+    y: float
+    year: int
+    properties: dict[Hashable, Any]
 
     class Config:
         orm_mode = True
