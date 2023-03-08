8c82d9b087bd6db92eb5565c76f54e0d424bc91d
Danny OBrien <danmcfan33@gmail.com>
2023-03-08 10:34:25 -0800

    update sleep task

--- python/planet_emu/celery/worker.py
+++ python/planet_emu/celery/worker.py
@@ -5,10 +5,14 @@ from celery import Celery
 
 broker = os.environ.get("CELERY_BROKER_URL", "redis://localhost:6379")
 backend = os.environ.get("CELERY_RESULT_BACKEND", "redis://localhost:6379")
-celery = Celery(__name__, broker=broker, backend=backend)
+
+celery = Celery(__name__, backend=backend, broker=broker)
 
 
 @celery.task
-def create_task(task_type) -> bool:
-    time.sleep(int(task_type) * 10)
-    return True
+def sleep(seconds: int) -> dict[str, bool | int]:
+    time.sleep(seconds)
+    return {
+        "complete": True,
+        "seconds": seconds,
+    }
