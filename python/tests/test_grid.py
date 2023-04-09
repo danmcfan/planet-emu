c680bee10d861f0bee5b859b05175720d31e7b05
Danny OBrien <danmcfan33@gmail.com>
2023-04-09 18:07:09 -0500

    change import

--- python/tests/test_grid.py
+++ python/tests/test_grid.py
@@ -1,18 +1,18 @@
 from tests.util import POLYGON
 
-from python.planet_emu import grid as gs
+from earth import grid
 
 SPLIT = 4
 COUNT = SPLIT**2
 
 
 def test_create_grid_gdf():
-    gdf = gs.create_grid_gdf(POLYGON, SPLIT)
+    gdf = grid.create_grid_gdf(POLYGON, SPLIT)
 
     assert gdf.shape == (COUNT, 1)
 
 
 def test_divide_polygon():
-    gdf = gs.divide_polygon(POLYGON, SPLIT)
+    gdf = grid.divide_polygon(POLYGON, SPLIT)
 
     assert gdf.shape == (COUNT, 1)
